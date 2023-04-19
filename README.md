ac8bc270cf80478131da057147408f4a545d9594
Rudra Saraswat <rs2009@ubuntu.com>
2023-04-19 22:41:23 +0530

    Shift base to Electron, add new slides

---
+++ README.md
@@ -0,0 +1,12 @@
+<div align="center">
+  <h1 align="center">blendOS First Setup</h1>
+  <p align="center">A first setup app designed for blendOS.</p>
+</div>
+
+## Running
+
+```sh
+npm i && npm start
+```
+
+(it's also available as `blendos-first-setup-git` in blendOS)
---
+++ main.js
@@ -0,0 +1,59 @@
+const { app, BrowserWindow, dialog, ipcMain } = require('electron')
+const { existsSync } = require('fs')
+
+var mainWindow = undefined
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    minWidth: 800,
+    minHeight: 600,
+    width: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webviewTag: true
+    }
+  })
+
+  let handler = (e) => {
+    e.preventDefault()
+
+    const choice = dialog.showMessageBox(mainWindow, {
+      type: 'question',
+      buttons: ['Cancel', 'Ok'],
+      title: 'Are you sure?',
+      message: "It's recommended that you go through the First Setup guide, even if you're an experienced user of other Linux distributions. First Setup will be shown again on the next login."
+    }).then((val) => {
+      if (val.response === 1) {
+        mainWindow.removeListener('close', handler)
+        app.quit()
+      }
+    })
+  }
+
+  mainWindow.on('close', handler)
+
+  ipcMain.on('app-quit', (e, msg) => {
+    mainWindow.removeListener('close', handler)
+
+    require('child_process').spawnSync('bash', ['-c', 'mkdir -p ~/.config; touch ~/.config/blendos-first-setup-done'])
+
+    app.quit()
+  })
+
+  mainWindow.setMenu(null)
+
+  mainWindow.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+  if (existsSync(require('os').homedir() + '/.config/blendos-first-setup-done')) {
+    app.quit()
+  } else {
+    setTimeout(createWindow, 2000)
+  }
+})
+
+app.on('window-all-closed', function () {
+  app.quit()
+})
